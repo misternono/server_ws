2e230b2af3474f4ba76ee9ac258bdf209cf9f3d3
DESKTOP-IOUBVN0\USER <w.noah.gabriel@gmail.com>
2022-10-12 09:32:36 +0200

    Add project files.

---
+++ server_ws/Handler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server_ws
+{
+    public class Handler
+    {
+        public async void Handle(Socket client)
+        {
+            byte[] option = new byte[4];
+            client.Receive(option);
+
+            var opt = Encoding.ASCII.GetString(option);
+
+            if (Encoding.ASCII.GetString(option) == "uplo")
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] rec = new byte[256];
+                    while (client.Receive(rec) != 0)
+                    {
+                        ms.Write(rec, 0, rec.Length);
+                    }
+                    var guid = Guid.NewGuid();
+                    File.WriteAllBytes(ConfigurationManager.AppSettings["download_dir"] + guid.ToString() + ".pdf", ms.ToArray());
+                }
+            }
+            else if (Encoding.ASCII.GetString(option) == "down")
+            {
+
+                byte[] file = null;
+                byte[] id = new byte[8];
+                client.Receive(id);
+                try
+                {
+                    file = System.IO.File.ReadAllBytes(ConfigurationManager.AppSettings["upload_dir"] + Encoding.ASCII.GetString(id, 0, id.TakeWhile(b => b != 0).Count()) + ".pdf");
+                }
+                catch (Exception e){
+                    file = Encoding.ASCII.GetBytes("ERROR: " + e.Message);
+                }
+
+                client.Send(file);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ server_ws/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace server_ws
+{
+    class Program
+    {
+        
+
+        static async Task Main(string[] args)
+        {
+            var ip = ConfigurationManager.AppSettings["server"];
+            var port = ConfigurationManager.AppSettings["port"];
+            TcpListener server = new TcpListener(IPAddress.Parse(ip), Int32.Parse(port));
+            server.Stop();
+            server.Start();
+            Console.WriteLine("Server has started on {0}:{1}\nWaiting for a connection…", ip, port);
+
+            for(; ; )
+            {
+                var client = server.AcceptSocket();
+                Console.WriteLine("A client connected.");
+                
+                Handler handler = new Handler();
+                handler.Handle(client);
+            }
+            server.Stop();
+
+        }
+    }
+}
